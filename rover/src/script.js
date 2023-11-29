3d87ce0875bd5ac43429284b6c7a2bf6b1882d0d
Fahad Alamgir Dhruba <dhruba.fahad2004@gmail.com>
2023-11-29 06:01:04 +0600

    nav

--- rover/src/script.js
+++ rover/src/script.js
@@ -1,16 +1,1 @@
 document.getElementById("copybox").style.display = "none";
-
-
-
-//script for dropdown starts 
-
-var dropbtn = document.getElementById("dropbtn");
-
-dropbtn.addEventListener("click", dropper);
-
-function dropper() {
-   var dropid = document.getElementById("dropid");
-   dropid.classList.toggle("hidden");
-}
-
-//script for dropdown starts 
